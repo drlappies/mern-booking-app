264c96af9b349fc88dcee40b68c6790a204d754c
drlappies <yau1chun2lap3@gmail.com>
2021-07-20 22:38:18 +0800

    refactor

--- backend/utils/isServiceAvailable.js
+++ backend/utils/isServiceAvailable.js
@@ -1,14 +1,13 @@
-const Appointment = require('../model/Appointment');
 const Room = require('../model/Room');
 
-const isServiceAvailable = async (roomId, serviceId, timeslot) => {
-    const isAvailable = await Appointment.find({ $and: [{ timeslot: { $eq: timeslot } }, { service: { $eq: serviceId } }] });
-    // check if the desired time slot for this service already has appointment
-    const room = await Room.findById(roomId);
-    if (isAvailable.length)
-        throw new Error('此日期時間已有預約'); // if the array is true means an existing booking is found, reject request.
-    if (new Date(timeslot).getHours() < room.openingHour || new Date(timeslot).getHours() > room.closingHour)
-        throw new Error('尚未開門或已經關門！');
+const isServiceAvailable = async (appointments, roomId) => {
+    const result = await Room.findById(roomId);
+    const hour = appointments.map(el => el.hour);
+    for (let i = 0; i < hour.length; i++) {
+        if (hour[i] < result.availability.operatingTime.openingTime || hour[i] > result.availability.operatingTime.closingTime) {
+            throw new Error('The room may be closed during this time!');
+        }
+    }
 }
 
 module.exports = isServiceAvailable
