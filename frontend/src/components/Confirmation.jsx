72206afe8f73cc1de6333186d89607b12963505c
drlappies <yau1chun2lap3@gmail.com>
2021-07-21 03:58:35 +0800

    booking confirmation

--- frontend/src/components/Confirmation.jsx
+++ frontend/src/components/Confirmation.jsx
@@ -1,12 +1,54 @@
 import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
 import { AppointmentContext } from './contexts/AppointmentContext';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const day = [
+    '星期日',
+    '星期一',
+    '星期二',
+    '星期三',
+    '星期四',
+    '星期五',
+    '星期六',
+]
 
 function Confirmation() {
-    const { selectedTimeslots, addTimeslots, removeTimeslots } = useContext(AppointmentContext)
+    const history = useHistory();
+    const { selectedTimeslots } = useContext(AppointmentContext);
+
     return (
         <Container>
-            <div>Confirmation</div>
+            <Grid container direction="row" justify="space-between" alignItems="center">
+                <Grid item>
+                    <Button startIcon={<ArrowBackIosIcon />} onClick={() => history.goBack()}>返回</Button>
+                </Grid>
+                <Grid item>
+                    <Button endIcon={<ArrowForwardIosIcon />}>確定並付款</Button>
+                </Grid>
+            </Grid>
+            <Grid container direction="column" justify="center" alignItems="center" spacing={2}>
+                <Grid item>
+                    <Typography variant="h5">確定已選擇時段</Typography>
+                </Grid>
+                <Grid item>
+                    <List>
+                        {selectedTimeslots.map((el, i) =>
+                            <ListItem>
+                                <ListItemText>{el.year}年{el.month}月{el.date}日 {day[el.day]} {el.hour}:00 到 {el.hour + 1}:00</ListItemText>
+                            </ListItem>
+                        )}
+                    </List>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
