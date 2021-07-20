68be4fc13bd81a21a09704538ed080899cb15b64
drlappies <yau1chun2lap3@gmail.com>
2021-07-20 22:38:40 +0800

    refactor

--- backend/utils/isTimeslotAvailable.js
+++ backend/utils/isTimeslotAvailable.js
@@ -1,6 +1,23 @@
-const isTimeslotAvailable = async (time) => {
-    const [timeslot] = await Timeslot.find({ time: { $eq: time } });
-    if (!timeslot.isAvailable) throw new Error('此時間日期已經被取！');
+const Appointment = require('../model/Appointment');
+
+const isTimeslotAvailable = async (appointments) => {
+    const year = appointments.map(el => el.year);
+    const month = appointments.map(el => el.month);
+    const date = appointments.map(el => el.date);
+    const hour = appointments.map(el => el.hour);
+    const service = appointments.map(el => el.service);
+    const room = appointments.map(el => el.room);
+    const [result] = await Appointment.find({
+        $and: [
+            { service: { $in: service } },
+            { room: { $in: room } },
+            { year: { $in: year } },
+            { month: { $in: month } },
+            { date: { $in: date } },
+            { hour: { $in: hour } },
+        ]
+    })
+    if (result) throw new Error('Either one timeslot or all has been taken！');
 }
 
 module.exports = isTimeslotAvailable
