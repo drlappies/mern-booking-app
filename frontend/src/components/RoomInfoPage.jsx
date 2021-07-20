93e2b436a3f07eefcad18c80a8b037c66917e22c
drlappies <yau1chun2lap3@gmail.com>
2021-07-21 03:59:02 +0800

    added opeartion time and day info

--- frontend/src/components/RoomInfoPage.jsx
+++ frontend/src/components/RoomInfoPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import Typography from '@material-ui/core/Typography';
@@ -21,6 +21,29 @@ const useStyles = makeStyles(({
     }
 }))
 
+const weekday = [
+    '日',
+    '一',
+    '二',
+    '三',
+    '四',
+    '五',
+    '六',
+]
+
+function checkIsDayOpen(availableWeekday) {
+    const availability = [];
+    let increment = 0;
+    for (let key in availableWeekday) {
+        if (availableWeekday[key]) {
+            availability.push(increment);
+        }
+        increment = increment + 1;
+    }
+
+    return availability;
+}
+
 function RoomInfoPage() {
     const classes = useStyles()
     const { id } = useParams();
@@ -28,12 +51,14 @@ function RoomInfoPage() {
     const [review, setReview] = useState([]);
     const [img, setImg] = useState([]);
     const [service, setService] = useState([]);
+    const [availableWeekday, setAvailableWeekday] = useState([]);
+    const [openingTime, setOpeningTime] = useState(9);
+    const [closingTime, setClosingTime] = useState(23);
     const [sort, setSort] = useState('earliest');
 
     useEffect(() => {
         fetchRoom()
-        console.log('render')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const fetchRoom = async () => {
@@ -42,17 +67,22 @@ function RoomInfoPage() {
         setReview(res.data.reviews)
         setImg(res.data.image.imageUrl)
         setService(res.data.services)
+        setAvailableWeekday(res.data.availability.weekday);
+        setOpeningTime(res.data.availability.operatingTime.openingTime);
+        setClosingTime(res.data.availability.operatingTime.closingTime);
     }
 
     const handleChange = (event) => {
         setSort(event.target.value)
         setReview(review.reverse())
     }
-
+    const availability = useMemo(() => checkIsDayOpen(availableWeekday), [availableWeekday])
     return (
         <Container>
             <Typography variant="h3">{header.title}</Typography>
             <Typography variant="h6">{header.description}</Typography>
+            <Typography variant="subtitle1">營業時間：{openingTime}:00 - {closingTime}:00</Typography>
+            <Typography variant="subtitle1">營業日子：逢星期{availability.map((el, i) => <span key={i}>&nbsp;{weekday[el]}&nbsp;</span>)}</Typography>
             <Hidden only={['xl', 'lg', 'md']}>
                 <ImageSlide imgArr={img} />
             </Hidden>
