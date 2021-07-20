b5a3345120bfa8f546030cf1b999f451e6096255
drlappies <yau1chun2lap3@gmail.com>
2021-07-20 22:41:38 +0800

    fix

--- frontend/src/components/contexts/AppointmentContext.jsx
+++ frontend/src/components/contexts/AppointmentContext.jsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from 'react';
 import { useSnackbar } from 'notistack';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export const AppointmentContext = createContext();
 
@@ -7,28 +9,47 @@ export function AppointmentProvider(props) {
     const [selectedTimeslots, setSelectedTimeslots] = useState([]);
     const { enqueueSnackbar } = useSnackbar();
 
-    const addTimeslot = (year, month, date, hour) => {
-        enqueueSnackbar(`已加入 ${year}年${month}月${date}日 ${hour}:00 - ${hour + 1}:00`, { variant: 'success', autoHideDuration: 3000 })
+    const addTimeslot = (year, month, date, hour, day) => {
+        enqueueSnackbar(`已加入時段 ${year}年${month}月${date}日 ${hour}:00 - ${hour + 1}:00`, { variant: 'success', autoHideDuration: 3000 })
         setSelectedTimeslots(prevState =>
             prevState.concat({
                 hour: hour,
                 year: year,
                 month: month,
-                date: date
+                date: date,
+                day: day
             })
         )
     }
 
-    const removeTimeslot = (year, month, date, hour) => {
-        enqueueSnackbar(`已取消加入 ${year}年${month}月${date}日 ${hour}:00 - ${hour + 1}:00`, { variant: 'error', autoHideDuration: 3000 })
-        const index = selectedTimeslots.findIndex(el => el.hour === hour && el.year === year && el.date === date && el.month === month)
+    const removeTimeslot = (year, month, date, hour, day) => {
+        enqueueSnackbar(`已取消加入時段 ${year}年${month}月${date}日 ${hour}:00 - ${hour + 1}:00`, { variant: 'error', autoHideDuration: 3000 })
+        const index = selectedTimeslots.findIndex(el => el.hour === hour && el.year === year && el.date === date && el.month === month && el.day === day)
         setSelectedTimeslots(prevState =>
             prevState.filter((el, i) => i !== index)
         )
     }
 
+    const bookTimeslot = async (roomId, serviceId, timeslots) => {
+        try {
+            const payload = {
+                appointments: timeslots.map(el => ({
+                    service: serviceId,
+                    room: roomId,
+                    year: el.year,
+                    month: el.month,
+                    date: el.date,
+                    hour: el.hour
+                }))
+            }
+            await axios.post(`/room/${roomId}/service/${serviceId}/appointment`, payload);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
-        <AppointmentContext.Provider value={{ selectedTimeslots, addTimeslot, removeTimeslot }}>
+        <AppointmentContext.Provider value={{ selectedTimeslots, addTimeslot, removeTimeslot, bookTimeslot }}>
             {props.children}
         </AppointmentContext.Provider>
     )
