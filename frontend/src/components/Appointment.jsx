416975f3865c0d72abe2bfca120f10808da59780
drlappies <yau1chun2lap3@gmail.com>
2021-07-21 03:57:43 +0800

    removed room info in booking page

--- frontend/src/components/Appointment.jsx
+++ frontend/src/components/Appointment.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect, useMemo, useContext } from 'react';
+import { AppointmentContext } from './contexts/AppointmentContext';
+import { Link, useParams, useLocation, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import Container from '@material-ui/core/Container';
 import Calendar from './Calendar';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const weekday = [
     '日',
@@ -30,33 +33,28 @@ function checkIsDayOpen(availableWeekday) {
 }
 
 function Appointment() {
+    const { pathname } = useLocation();
+    const history = useHistory();
+    const { selectedTimeslots } = useContext(AppointmentContext)
     const [openingTime, setOpeningTime] = useState(9);
     const [closingTime, setClosingTime] = useState(23);
-    const [roomName, setRoomName] = useState('');
-    const [serviceName, setServiceName] = useState('');
-    const [serviceRemark, setServiceRemark] = useState('');
-    const [servicePrice, setServicePrice] = useState(0);
     const [availableWeekday, setAvailableWeekday] = useState([]);
     const [appointments, setAppointments] = useState([]);
     const { roomId, serviceId } = useParams();
 
     useEffect(() => {
         fetchData()
+        window.scrollTo(0, 0);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [pathname])
 
     const fetchData = async () => {
         try {
             const room = await axios.get(`/room/${roomId}`);
             const appointments = await axios.get(`/room/${roomId}/service/${serviceId}/appointment`)
-            console.log(appointments)
             setAppointments(appointments.data)
             setOpeningTime(room.data.availability.operatingTime.openingTime);
             setClosingTime(room.data.availability.operatingTime.closingTime);
-            setRoomName(room.data.title);
-            setServiceName(room.data.services.find(el => el._id === serviceId).name)
-            setServiceRemark(room.data.services.find(el => el._id === serviceId).remark)
-            setServicePrice(room.data.services.find(el => el._id === serviceId).pricing)
             setAvailableWeekday(room.data.availability.weekday);
         } catch (err) {
             console.log(err)
@@ -67,17 +65,12 @@ function Appointment() {
 
     return (
         <Container>
-            <Grid container direction="row" spacing={1}>
-                <Grid item xs={12}>
-                    <Typography variant="h4">{roomName}</Typography>
-                    <Typography display="inline" variant="h5">{serviceName}</Typography>
-                    <Typography display="inline"> - </Typography>
-                    <Typography display="inline" variant="subtitle1">{serviceRemark}</Typography>
+            <Grid container direction="row" justify="space-between" alignItems="center" spacing={1}>
+                <Grid item>
+                    <Button startIcon={<ArrowBackIosIcon />} onClick={() => history.goBack()}>返回</Button>
                 </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">營業日子：逢星期{availability.map((el, i) => <span key={i}>&nbsp;{weekday[el]}&nbsp;</span>)}</Typography>
-                    <Typography variant="subtitle1">營業時間：{openingTime}:00 - {closingTime}:00</Typography>
-                    <Typography variant="subtitle1">收費：{servicePrice} / 小時</Typography>
+                <Grid item>
+                    <Button disabled={selectedTimeslots <= 0} component={Link} to={`/room/${roomId}/service/${serviceId}/appointment/confirmation`} endIcon={<ArrowForwardIosIcon />}>繼續</Button>
                 </Grid>
                 <Grid item xs={12}>
                     <Calendar
