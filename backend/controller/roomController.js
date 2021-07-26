6d97e005fabce9c25160cef210a7ea0a728ad30d
drlappies <yau1chun2lap3@gmail.com>
2021-07-26 09:21:18 +0800

    room update route

--- backend/controller/roomController.js
+++ backend/controller/roomController.js
@@ -61,8 +61,8 @@ module.exports.createRoom = async (req, res, next) => {
             closingTime: closingTime
         });
         req.files.forEach(el => {
-            newRoom.image.imageUrl.push(el.location);
-            newRoom.image.imageKey.push(el.key)
+            newRoom.imageUrl.push(el.location);
+            newRoom.imageKey.push(el.key)
         })
         await newRoom.save()
         res.json(newRoom)
@@ -73,19 +73,59 @@ module.exports.createRoom = async (req, res, next) => {
 
 module.exports.editRoom = async (req, res, next) => {
     try {
-        const updatedRoom = await Room.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (req.file) {
-            const imageKey = updatedRoom.image.imageKey[0];
-            imageDestroy(imageKey);
-            updatedRoom.image.imageKey = [];
-            updatedRoom.image.imageUrl = [];
-            updatedRoom.image.imageUrl.push(req.file.location);
-            updatedRoom.image.imageKey.push(req.file.key);
+        const { monday, tuesday, wednesday, thursday, friday, saturday, sunday, title, description, street, floor, flat, building, region, openingTime, closingTime, deletedImages } = req.body
+        const { id } = req.params
+        const updateRoom = await Room.findByIdAndUpdate(id, {
+            owner: req.user.id,
+            title: title,
+            description: description,
+            address: {
+                street: street,
+                floor: floor,
+                flat: flat,
+                building: building,
+                region: region,
+            },
+            openWeekday: {
+                monday: monday,
+                tuesday: tuesday,
+                wednesday: wednesday,
+                thursday: thursday,
+                friday: friday,
+                saturday: saturday,
+                sunday: sunday
+            },
+            openingTime: openingTime,
+            closingTime: closingTime
+        }, { new: true })
+        if (req.files) {
+            req.files.forEach(el => {
+                updateRoom.imageUrl.push(el.location);
+                updateRoom.imageKey.push(el.key)
+            })
         }
-        await updatedRoom.save();
-        res.json(updatedRoom)
+        if (deletedImages) {
+            const deletekeys = JSON.parse(deletedImages).map(el => ({ Key: el.key }));
+            const urls = JSON.parse(deletedImages).map(el => el.url);
+            const keys = JSON.parse(deletedImages).map(el => el.key);
+            imageDestroy(deletekeys);
+            await updateRoom.updateOne({ $pull: { imageKey: { $in: keys } } })
+            await updateRoom.updateOne({ $pull: { imageUrl: { $in: urls } } })
+        }
+        res.json(updateRoom)
     } catch (err) {
-        res.status(422)
+        next(err)
+    }
+}
+
+module.exports.roomManagement = async (req, res, next) => {
+    try {
+        const { id } = req.user;
+        const rooms = await Room.find({})
+            .populate('services')
+            .where('owner').equals(id)
+        res.json(rooms)
+    } catch (err) {
         next(err)
     }
 }
