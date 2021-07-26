2d8118272044db7fa00e469c3aefb168b4281080
drlappies <yau1chun2lap3@gmail.com>
2021-07-26 09:29:14 +0800

    fix

--- frontend/src/components/contexts/AuthenticationContext.jsx
+++ frontend/src/components/contexts/AuthenticationContext.jsx
@@ -9,6 +9,7 @@ export function AuthenticationProvider(props) {
     const history = useHistory();
     const { enqueueSnackbar } = useSnackbar();
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isOwner, setIsOwner] = useState(false);
 
     const handleLogout = () => {
         window.localStorage.removeItem('token')
@@ -17,9 +18,10 @@ export function AuthenticationProvider(props) {
         history.push('/')
     }
 
-    const handleRegister = async (username, password, title, permission) => {
+    const handleRegister = async (email, username, password, title, permission) => {
         try {
             const payload = {
+                email: email,
                 username: username,
                 password: password,
                 permission: permission,
@@ -29,6 +31,7 @@ export function AuthenticationProvider(props) {
             const res = await axios.post('/user/login', payload, { withCredentials: true });
             window.localStorage.setItem('token', res.data.token);
             setIsAuthenticated(true);
+            setIsOwner(res.data.permission)
             history.push('/')
         } catch (err) {
             enqueueSnackbar(`${err.response.data}`, { variant: 'error', autoHideDuration: 1500, anchorOrigin: { vertical: 'top', horizontal: 'center' }, preventDuplicate: true })
@@ -44,24 +47,29 @@ export function AuthenticationProvider(props) {
             const res = await axios.post('/user/login', payload, { withCredentials: true });
             window.localStorage.setItem('token', res.data.token);
             setIsAuthenticated(true);
-            enqueueSnackbar(`你好！${res.data.user.username}`, { variant: 'success', autoHideDuration: 1500, anchorOrigin: { vertical: 'top', horizontal: 'center' }, preventDuplicate: true })
+            setIsOwner(res.data.permission);
+            enqueueSnackbar(`你好！${res.data.username}`, { variant: 'success', autoHideDuration: 1500, anchorOrigin: { vertical: 'top', horizontal: 'center' }, preventDuplicate: true })
             history.goBack();
         } catch (err) {
             enqueueSnackbar(`${err.response.data}`, { variant: 'error', autoHideDuration: 1500, anchorOrigin: { vertical: 'top', horizontal: 'center' }, preventDuplicate: true })
         }
     }
 
-    const checkValidity = async () => {
+    const checkPermission = async (permission) => {
         try {
-            await axios.get('/user', { headers: { 'x-auth-token': window.localStorage.getItem('token') } })
+            const res = await axios.get('/user', { headers: { 'x-auth-token': window.localStorage.getItem('token') } });
+            if (!res.data.permission) {
+                history.push('/')
+                enqueueSnackbar('權限不足', { variant: 'warning', autoHideDuration: 1500, anchorOrigin: { vertical: 'top', horizontal: 'center' }, preventDuplicate: true })
+            }
         } catch (err) {
             enqueueSnackbar(`${err.response.data}`, { variant: 'warning', autoHideDuration: 1500, anchorOrigin: { vertical: 'top', horizontal: 'center' }, preventDuplicate: true })
-            history.push('/login')
+            history.push('/')
         }
     }
 
     return (
-        <AuthenticationContext.Provider value={{ isAuthenticated, handleLogout, handleLogin, handleRegister, checkValidity }}>
+        <AuthenticationContext.Provider value={{ isAuthenticated, handleLogout, handleLogin, handleRegister, isOwner, checkPermission }}>
             {props.children}
         </AuthenticationContext.Provider>
     )
