3922c63af3638e2ce7f93f9dd1a0a0f8846b06b3
drlappies <yau1chun2lap3@gmail.com>
2021-07-29 00:35:29 +0800

    unused middleware remove

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -16,29 +16,3 @@ module.exports.isAuthorised = async (req, res, next) => {
         next(err)
     }
 }
-
-module.exports.isRoomOwner = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.user.id);
-        if (!user.ownedRooms.includes(req.params.id) || !user.isRoomOwner) {
-            res.status(401)
-            throw new Error('權限不足');
-        }
-        next()
-    } catch (err) {
-        next(err)
-    }
-}
-
-module.exports.isReviewAuthor = async (req, res, next) => {
-    try {
-        const review = await Review.findById(req.params.reviewId);
-        if (req.user.id != review.author) {
-            res.status(401)
-            throw new Error('權限不足');
-        }
-        next()
-    } catch (err) {
-        next(err)
-    }
-}
