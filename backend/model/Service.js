7107999aca8269ae05c4b2607b6eb257669be0fd
drlappies <yau1chun2lap3@gmail.com>
2021-07-29 00:34:03 +0800

    middleware to remove from room model - service

--- backend/model/Service.js
+++ backend/model/Service.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const Room = require('../model/Room');
 
 const serviceSchema = new Schema({
     name: {
         type: String,
         required: [true, '請至少提供一項服務！']
+    },
+    room: {
+        type: Schema.Types.ObjectId,
+        ref: 'Room',
+        required: true
     },
     pricing: {
         type: Number,
@@ -18,7 +24,21 @@ const serviceSchema = new Schema({
     },
     remark: {
         type: String,
+    },
+    isOnline: {
+        type: Boolean,
+        required: true
     }
+})
+
+serviceSchema.post('findOneAndDelete', async function (data) {
+    await Room.findByIdAndUpdate(data.room, {
+        $pull: {
+            services: {
+                $in: data._id
+            }
+        }
+    })
 })
 
 module.exports = mongoose.model('Service', serviceSchema);
