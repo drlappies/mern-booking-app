4ba65b27d077d92a19e34578942674e62c8612a2
drlappies <yau1chun2lap3@gmail.com>
2021-08-01 04:53:10 +0800

    payment logic

---
+++ backend/controller/transactionController.js
@@ -0,0 +1,58 @@
+const Owner = require('../model/Owner');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Service = require('../model/Service')
+
+module.exports.onboard = async (req, res, next) => {
+    try {
+        const { id } = req.user
+        const owner = await Owner.findById(id)
+        const accountLinks = await stripe.accountLinks.create({
+            account: owner.stripe_id,
+            refresh_url: 'http://localhost:3000',
+            return_url: 'http://localhost:3000',
+            type: 'account_onboarding'
+        })
+        res.json({
+            url: accountLinks.url
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports.getOnboardStatus = async (req, res, next) => {
+    try {
+        const { id } = req.user;
+        const owner = await Owner.findById(id)
+        const account = await stripe.accounts.retrieve(owner.stripe_id);
+        res.json({
+            isOnboarded: account.charges_enabled && account.details_submitted ? true : false
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports.getPaymentIntent = async (req, res, next) => {
+    try {
+        const { roomId, serviceId, appointments, pricing } = req.body;
+        const service = await Service.findById(serviceId)
+            .populate('owner');
+        if (service.pricing !== pricing) return res.json('price has been manipulated')
+        const { stripe_id } = service.owner
+        const paymentIntent = await stripe.paymentIntents.create({
+            payment_method_types: ['card'],
+            amount: appointments.length * pricing * 100,
+            currency: 'hkd',
+            application_fee_amount: Math.round(appointments.length * pricing * 0.05)
+        }, {
+            stripeAccount: stripe_id
+        });
+        res.json({
+            client_secret: paymentIntent.client_secret,
+            connected_stripe_account_id: stripe_id
+        })
+    } catch (err) {
+        next(err)
+    }
+}
