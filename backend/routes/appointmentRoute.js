a21293a09690e48960ea81f2c524e6246502d1e7
drlappies <yau1chun2lap3@gmail.com>
2021-08-01 04:55:26 +0800

    refactor

--- backend/routes/appointmentRoute.js
+++ backend/routes/appointmentRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const appointmentController = require('../controller/appointmentController');
-const { isLoggedIn } = require('../utils/middleware');
+const { isAuthorised } = require('../utils/middleware')
 
 router.get('/', appointmentController.getAppointment);
 
-router.post('/', appointmentController.makeAppointment);
+router.post('/', isAuthorised, appointmentController.makeAppointment);
 
 module.exports = router;
