db7a1541a7ad6e2bf6c707805a33ca7b17760ef4
drlappies <yau1chun2lap3@gmail.com>
2021-08-01 04:51:19 +0800

    fix

--- backend/utils/isServiceAvailable.js
+++ backend/utils/isServiceAvailable.js
@@ -4,7 +4,7 @@ const isServiceAvailable = async (appointments, roomId) => {
     const result = await Room.findById(roomId);
     const hour = appointments.map(el => el.hour);
     for (let i = 0; i < hour.length; i++) {
-        if (hour[i] < result.availability.operatingTime.openingTime || hour[i] > result.availability.operatingTime.closingTime) {
+        if (hour[i] < result.operatingTime || hour[i] > result.operatingTime) {
             throw new Error('The room may be closed during this time!');
         }
     }
