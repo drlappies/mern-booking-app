e292a796158ab21f69cf10e026bff918d187d7ea
drlappies <yau1chun2lap3@gmail.com>
2021-09-23 23:20:20 +0800

    fixed incorrect lettercase that caused docker bug

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { AuthenticationContext } from './contexts/AuthenticationContext'
 import { NavLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/Appbar';
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
